578c7b34c6caf3a5e98556cd7d5c575841348159
Ramon Brand <ramon2009@gmail.com>
2017-05-14 14:04:43 +0300

    Testing script deploy.

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log("Running node app.");
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World woohoo!\n');
+  res.end('Hello World - Change 1!\n');
 }).listen(8080);
